c7c7bd3193ffd8edb2101df7a042f67db768b6ec
e4ma-okafor <ifeomasandra13@gmail.com>
2023-08-16 18:48:59 -0700

    Added some files

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+
+#endif
